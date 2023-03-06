dcb5df0a61adca1b54136d2e1579bd5bdd01ef96
Tu Nguyen Pham <tu.nguyenpham@coxautoinc.com>
2023-03-06 13:31:54 +0700

    initialize repo

---
+++ src/lambda/http/createGroup.ts
@@ -0,0 +1,35 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const groupsTable = process.env.GROUPS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Processing event: ', event);
+    const itemId = uuid.v4()
+    console.log('new id: ', itemId);
+    const parsedBody = JSON.parse(event.body);
+
+    const newItem = {
+        id: itemId,
+        ...parsedBody
+    };
+    console.log('table: ', groupsTable);
+    console.log('newItem: ',newItem);
+  await docClient.put({
+    TableName: groupsTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
+}
