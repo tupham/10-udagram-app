4bc0052d95508cbb39753e2a1a2d64e961f57964
Tu Nguyen Pham <tu.nguyenpham@coxautoinc.com>
2023-03-07 10:23:59 +0700

    configure sendnotification from lambda to websocket successfully

---
+++ src/lambda/websocket/connect.ts
@@ -0,0 +1,20 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
+import * as AWS from 'aws-sdk';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const connectionsTable = process.env.CONNECTIONS_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> =>{
+    console.log('websocket connect', event);
+    const connectionId = event.requestContext.connectionId;
+    const timestamp = new Date().toISOString();
+    const item = {
+        id: connectionId,
+        timestamp
+    };
+    await docClient.put({TableName: connectionsTable, Item: item}).promise();
+    return {
+        statusCode: 200,
+        body: ''
+    }
+}
