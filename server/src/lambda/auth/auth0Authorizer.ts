f4335770f445bc184c8fdd50c21a803b9a56f7c3
Tu Nguyen Pham <tu.nguyenpham@coxautoinc.com>
2023-03-09 20:10:54 +0700

    authorize middleware lambda success

--- server/src/lambda/auth/auth0Authorizer.ts
+++ server/src/lambda/auth/auth0Authorizer.ts
@@ -1,24 +1,22 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
-
+import 'source-map-support/register';
+import * as middy from 'middy'
+import { secretsManager } from 'middy/middlewares'
 import {verify} from 'jsonwebtoken';
 import {JwtToken} from '../../auth/JwtToken';
-import * as AWS from 'aws-sdk';
 
 const secretId = process.env.AUTH_0_SECRET_ID;
 const secretField = process.env.AUTH_0_SECRET_FIELD;
 
-const client = new AWS.SecretsManager();
 
-let cachedSecret: string;
-
-export const handler = async(event: CustomAuthorizerEvent, context) : Promise<CustomAuthorizerResult>=>{
+export const handler = middy(async(event: CustomAuthorizerEvent, context) : Promise<CustomAuthorizerResult>=>{
     console.log('Authorize event: ', event);
     console.log('Authorize context: ', context);
     console.log('secretId:', secretId);
     console.log('secretField:', secretField);
     try {
 
-        const decodedToken = await verifyToken(event.authorizationToken);
+        const decodedToken = verifyToken(event.authorizationToken, context.AUTH0_SECRET[secretField]);
         console.log('User was authorized');
         var objectReturn = {
           principalId: decodedToken.sub,
@@ -38,9 +36,9 @@ export const handler = async(event: CustomAuthorizerEvent, context) : Promise<Cu
     }catch(e){
         console.log(e);
     }
-}
+});
 
-async function verifyToken(authHeader: string): Promise<JwtToken> {
+function verifyToken(authHeader: string, secret: string): JwtToken{
     if (!authHeader)
       throw new Error('No authentication header')
   
@@ -50,20 +48,16 @@ async function verifyToken(authHeader: string): Promise<JwtToken> {
     const split = authHeader.split(' ');
     const token = split[1];
     console.log('token: ', token);
-    const secretObject: any = await getSecret();
-    const secret = secretObject[1];
     console.log('secret: ', secret);
     return verify(token, secret) as JwtToken;
   }
   
-  async function getSecret(){
-    console.log('getSecret');
-    if(cachedSecret) return cachedSecret;
-    const data = await client.getSecretValue({
-      SecretId: secretId
-    }).promise();
-    console.log('data: ', data);
-    cachedSecret = data.SecretString;
-    var keyValue = cachedSecret.split('=');    
-    return keyValue;
-  }
+handler.use(
+  secretsManager({
+    cache: true,
+    cacheExpiryInMillis: 60000,
+    throwOnFailedCall: true,secrets:{
+      AUTH0_SECRET: secretId
+    }
+  })
+);
